e71856271c9951dcc935c755a4f5067a1b060865
baturay alhaj ahmad <thebatu@gmail.com>
2017-05-15 18:41:56 +0200

    add validations to models

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,9 @@
 class Cat < ApplicationRecord
+  has_many :bookings, dependent: :destroy
+
+  validates :name, format: { with: /\d?*[a-zA-Z]+\d*?/, message: "only allows letters" }, presence: true
+
+  validates :description, :address, presence: true
+
+  validates :price, numericality: { only_integer: true }, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -37,15 +37,6 @@ ActiveRecord::Schema.define(version: 20170515154903) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "booking_id"
-    t.integer  "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["booking_id"], name: "index_reviews_on_booking_id", using: :btree
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -67,5 +58,4 @@ ActiveRecord::Schema.define(version: 20170515154903) do
 
   add_foreign_key "bookings", "cats"
   add_foreign_key "bookings", "users"
-  add_foreign_key "reviews", "bookings"
 end
