6f2d406fb9d0211db7ffa8d43751918bd83b417c
Lucas Sotty <lucas.sotty@wanadoo.fr>
2017-05-15 18:15:40 +0200

    Created seed for cats table, only missing photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+puts "Destroying current cats database..."
+
+Cat.destroy_all
+
+100.times do
+  cat = Cat.new(
+    name: Faker::Cat.name,
+    description: Faker::Cat.breed,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.postcode}, #{Faker::Address.city}",
+    price: rand(10..20)
+  )
+  cat.save
+  print "#{cat.name} added to the database..."
+end
