126f6ca4db4a2b4c1efaa84a428eaac03980dcca
Pierre Paillere <peyop78@gmail.com>
2017-05-15 16:12:09 +0200

    added model Review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170515140728) do
+ActiveRecord::Schema.define(version: 20170515141137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 20170515140728) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "booking_id"
+    t.integer  "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -56,4 +65,5 @@ ActiveRecord::Schema.define(version: 20170515140728) do
 
   add_foreign_key "bookings", "cats"
   add_foreign_key "bookings", "users"
+  add_foreign_key "reviews", "bookings"
 end
