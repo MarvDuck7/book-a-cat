4ee3386717ca15e571101655dd4e2f3beb14eaea
Pierre Paillere <peyop78@gmail.com>
2017-05-15 17:45:50 +0200

    modified routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :cats, only: %i(index show), shallow: true do
+    resources :bookings, only: %i(index show create) do
+      resources :reviews, only: %i(create)
+    end
+  end
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
