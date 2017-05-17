7b4e2ac28a1a0b8a5b8aafbded3e76e416872ed5
Pierre Paillere <peyop78@gmail.com>
2017-05-17 14:11:31 +0200

    add geocode validation on Cat model

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -3,9 +3,11 @@ class Cat < ApplicationRecord
 
   has_attachment :photo
 
-  validates :name, length: {maximum: 50}, presence: true
+  validates :name, length: { maximum: 50 }, presence: true
 
   validates :description, presence: true
-  validates :address, length: {maximum: 560}, presence: true
+  validates :address, length: { maximum: 560 }, presence: true
   validates :price, numericality: { only_integer: true }, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 end
