41e34727fa0293e005291d5fb240b6c1dce52ab4
baturay alhaj ahmad <thebatu@gmail.com>
2017-05-18 17:19:08 +0200

    amend fb username if no username is returned from fb. use user.first_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,13 @@ class User < ApplicationRecord
   devise :omniauthable, omniauth_providers: [:facebook]
 
   def self.find_for_facebook_oauth(auth)
+
     user_params = auth.slice(:provider, :uid)
-    user_params.merge! auth.info.slice(:email)
+    user_params.merge! auth.info.slice(:email,:first_name)
     user_params[:facebook_picture_url] = auth.info.image
     user_params[:token] = auth.credentials.token
     user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
     user_params = user_params.to_h
-
     user = User.where(provider: auth.provider, uid: auth.uid).first
     user ||= User.where(email: auth.info.email).first # User did a regular sign up in the past.
     if user
@@ -27,7 +27,7 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0,20]  # Fake password for validation
       #if no username provided add a username
       if user.username.empty?
-        user.username = "fakeUsername"
+        user.username = "#{user.first_name}"
       end
       user.save
     end
