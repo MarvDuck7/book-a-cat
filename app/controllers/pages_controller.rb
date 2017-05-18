678fac4abb2330217cab514b85c12e472ea0fc14
Lucas Sotty <lucas.sotty@wanadoo.fr>
2017-05-18 17:58:06 +0200

    Fixed log out issue

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 class PagesController < ApplicationController
   def home
-    @number_of_pending_requests = 0
-    cats = current_user.cats
-    cats.each do |cat|
-      @number_of_pending_requests += cat.bookings.where("status = ?", "Pending").count
+    if user_signed_in?
+      @number_of_pending_requests = 0
+      cats = current_user.cats
+      cats.each do |cat|
+        @number_of_pending_requests += cat.bookings.where("status = ?", "Pending").count
+      end
     end
   end
 end
